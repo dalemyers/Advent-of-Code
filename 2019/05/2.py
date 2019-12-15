0cce79262be883d63ef2849e492813d30947b060
Dale Myers <dale@myers.io>
2019-12-15 17:25:06 +0000

    2019-05

---
+++ 2019/05/2.py
@@ -0,0 +1,139 @@
+import enum
+from typing import List, Optional
+
+class Opcode(enum.Enum):
+    ADD = 1
+    MULTIPLY = 2
+    INPUT = 3
+    OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
+    HALT = 99
+
+class ParameterMode(enum.Enum):
+    POSITION = 0
+    IMMEDIATE = 1
+
+class Program:
+
+    integers: List[int]
+    index: int
+
+    def __init__(self, integers: str) -> None:
+        self.integers = list(map(int, integers.split(",")))
+        self.index = 0
+
+    def get_next(self, parameter_mode: Optional[ParameterMode] = None) -> int:
+        value = self.integers[self.index]
+        self.index += 1
+
+        if not parameter_mode or parameter_mode == ParameterMode.IMMEDIATE:
+            return value
+        else:
+            return self.integers[value]
+
+    def get_value(self, index: int) -> int:
+        return self.integers[index]
+
+    def set_value(self, value: int, index: int) -> None:
+        self.integers[index] = value
+
+    def state(self) -> str:
+        return ",".join(map(str, self.integers))
+
+def run_program(input_list: List[int]) -> int:
+    with open("2019/05/input.txt") as f:
+        full_input = f.read().strip()
+
+    program = Program(full_input)
+
+    while True:
+        instruction_value = program.get_next()
+        opcode_value = instruction_value % 100
+        opcode = Opcode(opcode_value)
+        instruction_value -= opcode_value
+        instruction_value //= 100
+
+        parameter_modes = []
+
+        while instruction_value > 0:
+            mode_value = instruction_value % 10
+            instruction_value -= mode_value
+            instruction_value //= 10
+            parameter_modes.append(ParameterMode(mode_value))
+
+        parameter_lengths = {
+            Opcode.ADD: 2,
+            Opcode.MULTIPLY: 2,
+            Opcode.JUMP_IF_TRUE: 2,
+            Opcode.JUMP_IF_FALSE: 2,
+            Opcode.LESS_THAN: 2,
+            Opcode.EQUALS: 2,
+        }
+
+        # Make up implied parameters
+        parameter_length = parameter_lengths.get(opcode)
+        while parameter_length and len(parameter_modes) < parameter_length:
+            parameter_modes.append(ParameterMode.POSITION)
+
+        if opcode == Opcode.HALT:
+            break
+
+        if opcode == Opcode.JUMP_IF_TRUE:
+            value1 = program.get_next(parameter_modes[0])
+            value2 = program.get_next(parameter_modes[1])
+            if value1 != 0:
+                program.index = value2
+
+        if opcode == Opcode.JUMP_IF_FALSE:
+            value1 = program.get_next(parameter_modes[0])
+            value2 = program.get_next(parameter_modes[1])
+            if value1 == 0:
+                program.index = value2
+
+        if opcode == Opcode.LESS_THAN:
+            value2 = program.get_next(parameter_modes[1])
+            output_index = program.get_next()
+            if value1 < value2:
+                program.set_value(1, output_index)
+            else:
+                program.set_value(0, output_index)
+
+        if opcode == Opcode.EQUALS:
+
+            value2 = program.get_next(parameter_modes[1])
+            output_index = program.get_next()
+            if value1 == value2:
+                program.set_value(1, output_index)
+            else:
+                program.set_value(0, output_index)
+
+        if opcode == Opcode.INPUT:
+
+            output_index = program.get_next()
+            program.set_value(value, output_index)
+
+        if opcode == Opcode.OUTPUT:
+            index1 = program.get_next()
+            value1 = program.get_value(index1)
+            print(f"OUTPUT: {value1}")
+
+        if opcode == Opcode.ADD:
+            value1 = program.get_next(parameter_modes[0])
+            value2 = program.get_next(parameter_modes[1])
+            output_index = program.get_next()
+            output_value = value1 + value2
+            program.set_value(output_value, output_index)
+
+        if opcode == Opcode.MULTIPLY:
+            value1 = program.get_next(parameter_modes[0])
+            value2 = program.get_next(parameter_modes[1])
+            output_index = program.get_next()
+            output_value = value1 * value2
+            program.set_value(output_value, output_index)
+
+    return program.get_value(0)
+
+run_program([5])
